bd508ee42f881dd5bab526689bbc4a0be9f81c2f
Lipesaesa <felieptgonzalez07@gmail.com>
2025-09-23 16:33:48 -0300

    teste basico de camera

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,55 @@
-import { View } from "react-native";
-// import Main from "./src/screens/Main";
-import TesteAxios from "./src/screens/TesteAxios";
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, SafeAreaViewBase, TouchableOpacity } from 'react-native'
+import { Camera } from 'expo-camera'
+
 export default function App() {
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync(); // ou requestCameraPermissionsAsync()
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>Acesso negado!</Text>;
+  }
+
   return (
-    
-    //  <Main />
-    <TesteAxios />
-  );
+    <SafeAreaViewBase style={styles.container}>
+      <Camera style={{ flex: 1 }} type={type}>
+        <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              bottom: 20,
+              left: 20,
+            }}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              )
+            }}
+          >
+            <Text style={{ fontSize: 20, marginBottom: 13, color: '#FFF' }}>Trocar</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </SafeAreaViewBase>
+  )
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
