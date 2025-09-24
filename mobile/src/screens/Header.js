45974d6c45bfc011eb38e12468227baf40eff4be
VictorDumer <victorgabrieldummersouza@gmail.com>
2025-09-24 17:48:12 -0300

    w.i.p resumao do front-end

---
+++ mobile/src/screens/Header.js
@@ -0,0 +1,8 @@
+import { View, Image } from "react-native"
+import styles from "../styles/MainStyle"
+
+export default function Header(){
+    <View style={styles.logoContainer}>
+            <Image source={require('../../assets/top-logo.png')} style={styles.topLogo}/>
+    </View>
+}
